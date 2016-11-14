09cf10bd906ae7498c76d466a890b63a786826bf
Ese Okonofua <eseo@Esna.Main>
2016-11-14 16:23:55 -0500

    Commit!

--- src/containers/mainPage.js
+++ src/containers/mainPage.js
@@ -11,10 +11,13 @@ export default class MainPage extends Component {
                   <img src='https://s3-us-west-2.amazonaws.com/eseokonofua/images/IMG_0509.jpg'/>
                 </section>
                 <section id='bio'>
-                    <h2>Yo!</h2>
+                    <h2>Who art thou?</h2>
                     <p>
-                        I'm a biomedical computing student at Queen's University. Obsessed with the internet and in love with technology.
+                        I'm a biomedical computing student at Queen's University.
+                        Obsessed with the internet and in love with technology.
                     </p>
+                    <p>Constantly learning about the latest bleeding edge web tech.
+                    Working on multiple personal projects simultaneously.</p>
                 </section>
                 <section style={{backgroundColor:'#6f5468'}}></section>
                 <section style={{backgroundColor:'#f6bd9d'}}></section>
