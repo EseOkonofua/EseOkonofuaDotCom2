f614104e9a945a00db163f4495d60c99d16d7f60
Ese Okonofua <eseo@Esna.Main>
2016-11-16 16:28:45 -0500

    New blurb

--- src/containers/mainPage.js
+++ src/containers/mainPage.js
@@ -11,13 +11,16 @@ export default class MainPage extends Component {
                   <img src='https://s3-us-west-2.amazonaws.com/eseokonofua/images/IMG_0509.jpg'/>
                 </section>
                 <section id='bio'>
-                    <h2>Who art thou?</h2>
+                    <h2>Ese who?</h2>
+                    <p>I'm a biomedical computing student at Queen's University. Obsessed with the internet and in love with technology.</p>
+                    <p>Always keen on learning about the latest in bleeding edge web tech.</p>
+                    <p>I'm on a new path to physical, mental and intellectual growth.</p>
                     <p>
-                        I'm a biomedical computing student at Queen's University.
-                        Obsessed with the internet and in love with technology.
+                    Thus,<br/>
+                    I have developed a newly born addiction to Medium articles.
+                    My other addictions include anime, fitness, food and gaming.
                     </p>
-                    <p>Constantly learning about the latest bleeding edge web tech.
-                    Working on multiple personal projects simultaneously.</p>
+
                 </section>
                 <section style={{backgroundColor:'#6f5468'}}></section>
                 <section style={{backgroundColor:'#f6bd9d'}}></section>
