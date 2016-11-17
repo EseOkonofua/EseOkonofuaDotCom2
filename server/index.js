7029c0de99638b07e5b9cec79e3204c491af9229
Ese Okonofua <eseinyang2005@hotmail.com>
2016-11-17 07:32:06 -0500

    F

--- server/index.js
+++ server/index.js
@@ -48,7 +48,7 @@ app.get('*', function(req, res){
 
 
 
-var PORT = process.env.PORT || 4000;
+var PORT = process.env.PORT || 3000;
 
 app.listen(PORT, function(){
 	console.log("listening on port", PORT);
